14ded7531f580f3c0d1bbd8044f828a0dfe56fa2
NilsenLukas <lukas.nilsen2021lax@gmail.com>
2026-01-30 08:42:15 -0500

    updating metadata

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,24 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
-  // other metadata
+  title: "SmoqueSignals BBQ",
+  description: "This is Home for SmoqueSignals BBQ",
+  
+  openGraph: {
+    title: "Smoque Signals BBQ",
+    description: "This is Home for SmoqueSignals BBQ",
+    siteName: "Smoque Signals BBQ",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Smoque Signals BBQ food truck and smoked meats",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function Home() {
